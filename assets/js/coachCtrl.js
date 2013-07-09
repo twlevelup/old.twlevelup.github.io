1a27dad8c84fa08b58032c2f22ac5e6bfe660fd4
Peter Budd <pbudd@thoughtworks.com>
2013-07-09 21:08:52 +1000

    Peter, implemented angular js on the pages where haml isn't working

---
+++ assets/js/coachCtrl.js
@@ -0,0 +1,15 @@
+function coachCtrl($scope) {
+  $scope.coaches = [
+    {
+      name: "Sam Gibson",
+      image: "/assets/img/coaches/sam-gibson.jpg",
+      blurb: "Man of Steel"
+    },
+
+    {
+      name: "Pete Chemsripong",
+      image: "http://www.placekitten.com/200/300",
+      blurb: "<h1>What a man</h1> <p>Something about Pete</p>"
+    }
+  ];
+}
---
+++ assets/js/eventCtrl.js
@@ -0,0 +1,35 @@
+function eventCtrl($scope) {
+  $scope.events = [
+    {
+      week: "Week 1",
+      summary: "For the first week, we're going to learn about My Little Pony",
+      description: "My Little Pony is an entertainment franchise developed by Hasbro which is marketed primarily to girls. It started as a line of plastic pony toys developed by Bonnie Zacherle, Charles Muenchinger and Steve D'Aguanno which have been produced since 1983. The ponies feature colorful bodies, manes and a unique symbol on one or both sides of their flanks. These are referred to in the two most recent generations as \"cutie marks.\" My Little Pony was revamped at least four times with new and more modern looks to appeal to a new market.",
+      preReading: [
+        {
+          link: "www.youtube.com/watch?v=GI2fP2Z_Wmo‎",
+          title: "My Little Pony on Youtube"
+        },
+        {
+          link: "www.hasbro.com/mylittlepony/en_AU/",
+          title: "My Little Pony Official Site"
+        }
+      ]
+    },
+
+    {
+      week: "Week 2",
+      summary: "For the second week, we will discuss the physics that underpins Superman, the man of steel",
+      description: "Superman is kinda bullshit, and is an 8 years old's idea of a superhero as he does everything well and nothing can hurt him, makes for interesting television (not)",
+      preReading: [
+        {
+          link: "http://www.youtube.com/watch?v=T6DJcgm3wNY",
+          title: "Man of Steel trailer"
+        },
+        {
+          link: "http://www.rottentomatoes.com/m/superman_man_of_steel/",
+          title: "Rotten tomatoes review"
+        }
+      ]
+    }
+  ];
+}
