bfd53856bf86715ad0a1d56df60762a412db5e5a
MasterPete <masterpetemaniac@gmail.com>
2013-07-10 19:27:24 +1000

    Update contents

--- assets/js/coachCtrl.js
+++ assets/js/coachCtrl.js
@@ -2,14 +2,151 @@ function coachCtrl($scope) {
   $scope.coaches = [
     {
       name: "Sam Gibson",
+      title: "Man of American Steel",
       image: "/assets/img/coaches/sam-gibson.jpg",
-      blurb: "Man of Steel"
+      blurb: "<p>My Little Pony is an entertainment franchise developed by Hasbro which is marketed primarily to girls. It started as a line of plastic pony toys developed by Bonnie Zacherle, Charles Muenchinger and Steve D'Aguanno which have been produced since 1983.</p> <p>As a result of these qualities, Friendship Is Magic has not only proven a major success in its intended demographics,[6] but also unexpectedly gained significant followings in peripheral ones, such as teen and adult males. The show has also attracted media coverage for its fanbase, which has spawned numerous adult-themed parodies, mashups, and images, mostly outside its main target audience, especially seen on YouTube and DeviantArt.</p>"
+    },
+    {
+      name: "Sam Massey",
+      title: "Grad Recruiter/Actor/Writer/Thought wanderer and keeper of the peace",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Speaking at this year's AAGE annual conference about why we became no.1 grad employer in Australia...I'm still figuring it out!</p> <p>Grew up in a teen weeny village in England. Trained as an actor, got paid for 10 years to speak Shakespeare and entertain people, became a writer, joined ThoughtWorks London part-time in 2010, loved it, became grad recruiter in Oz in 2013, loved it even more. Still write/film stuff in my spare time. Lover of football (the European kind). Photographer and ocean swimmer. </p>"
+    },
+    {
+      name: "Darren Smith",
+      title: "Architect, Developer, and Principal Consultant",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Over 18 years experience in the IT industry, across Australia, China, USA and Canada. As a principal consultant with ThoughtWorks Australia, Darren possesses a unique ability to balance strategic thinking with delivering solutions that meet immediate needs.</p> <p>Specialties: Consulting, agile software development, and enterprise architecture.</p>"
+    },
+    {
+      name: "Gareth Sutton",
+      title: "Sales Master",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Consultative sales rep, looking to help customers find business and technical improvement. </p> <p>ThoughtWorks are the leaders in Agile, trying to change the way technology is delivered. Our consulting services range from developing awesome software through to helping you change the way you innovate. </p>"
+    },
+    {
+      name: "Angus Irvine",
+      title: "Senior Consultant at ThoughtWorks",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Agile Business Analyst with considerable experience in web projects across both the commercial product and business application spaces.</p> <p>Specialties:Requirements elicitation, user story creation, workshop facilitation, stakeholder management, iteration management, agile delivery.</p>"
+    },
+    {
+      name: "Adam Hope",
+      title: "Grad Recruiter/Actor/Writer/Thought wanderer and keeper of the peace",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Highly experienced Client Side Web Developer with strong skills in Web 2.0 technologies. A proven track record in delivering complex client side projects. Excellent reputation as an effective, reliable, results-oriented developer and team player.</p> <p>Particular expertise in building standards compliant and accessible user interface frameworks and components for a variety of web applications using XML, XSLT, HTML, JavaScript and CSS.</p> <p>Specialties:AJAX, Javascript, CSS, HTML, XML, XSLT, XHTML, Graphic Design, W3C AAA Compliance</p>"
+    },
+    {
+      name: "James Gregory",
+      title: "Senior Consultant at ThoughtWorks",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Code monkey. Breaker, fixer, do'er.</p>"
+    },
+      {
+          name: "Julian Parry",
+          title: "Batman",
+          image: "http://www.placekitten.com/150/150",
+          blurb: "<p>Hey now hey now</p>"
+      },
+    {
+      name: "Andrew Jones",
+      title: "Software Developer",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Specialties:Software development, GNU/Linux systems administration, web development.</p>"
+    },
+    {
+      name: "Cassandra Hill",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Kate Wilson",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Peter Budd",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Sam Newman",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Alex Gibson",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Evgany Dudin",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Sam Massey",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Sam Massey",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Inny So",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Leonor Salazar",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Keith Wale",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Khali Young",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Sam Massey",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
+    },
+    {
+      name: "Marina Chiovetti",
+      title: "Batman",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Hey now hey now</p>"
     },
 
     {
       name: "Pete Chemsripong",
-      image: "http://www.placekitten.com/200/300",
-      blurb: "<h1>What a man</h1> <p>Something about Pete</p>"
+      title: "Professional Doodler",
+      image: "http://www.placekitten.com/150/150",
+      blurb: "<p>Pete has over 9 years of experience in the field of design - Freelance illustrator, Children art teacher, Comic artist, Sold-out corporate graphic designer, Web designer, Team lead, you name it. His current role as an Experience Designer get him close and personal with his passion - the intersection of art (being human) and technology. &quot;The Holy Grail&quot;, he said.</p> <p>Pete's art work was featured at the Art Gallery of NSW. His comic book can probably still be found from Dark Horse, despite him trying to erase everything about that project.</p> <p>And yes, he also code.</p>"
     }
+
+
+
   ];
 }
--- assets/js/programCtrl.js
+++ assets/js/programCtrl.js
@@ -2,8 +2,8 @@ function programCtrl($scope) {
   $scope.programs = [
     {
       week: "Week 1",
-      summary: "For the first week, we're going to learn about My Little Pony",
-      description: "My Little Pony is an entertainment franchise developed by Hasbro which is marketed primarily to girls. It started as a line of plastic pony toys developed by Bonnie Zacherle, Charles Muenchinger and Steve D'Aguanno which have been produced since 1983. The ponies feature colorful bodies, manes and a unique symbol on one or both sides of their flanks. These are referred to in the two most recent generations as \"cutie marks.\" My Little Pony was revamped at least four times with new and more modern looks to appeal to a new market.",
+      summary: "Introduction / Agile Fundamentals / Team forming",
+      description: "<p>Welcome to the program! We'll start by showing you how software development works in the real world - what's good, what's bad, and why we're all about agile. </p> <p>Once we're all grounded, we'll introduce you to the project you'll be working on (spoiler - its a video game), and we'll the teams that you'll be delivering in!</p>",
       preReading: [
         {
           link: "www.youtube.com/watch?v=GI2fP2Z_Wmo‎",
@@ -18,8 +18,113 @@ function programCtrl($scope) {
 
     {
       week: "Week 2",
-      summary: "For the second week, we will discuss the physics that underpins Superman, the man of steel",
-      description: "Superman is kinda bullshit, and is an 8 years old's idea of a superhero as he does everything well and nothing can hurt him, makes for interesting television (not)",
+      summary: "Language fundamentals",
+      description: "New to coding in X? We'll show you the ropes, and then its time to get your hands dirty. Not a coder? No problem - we'll take you over to the dark side and show you the fundamentals of analysis, design and project management.",
+      preReading: [
+        {
+          link: "http://www.youtube.com/watch?v=T6DJcgm3wNY",
+          title: "Man of Steel trailer"
+        },
+        {
+          link: "http://www.rottentomatoes.com/m/superman_man_of_steel/",
+          title: "Rotten tomatoes review"
+        }
+      ]
+    },
+    {
+      week: "Week 3",
+      summary: "Testing fundamentals / User testing",
+      description: "Quality software is about more than just great code. From unit testing to user testing, we'll teach you the value of a test first philosophy, and show you how to put it into practice. Not a tester? Yes you are! Ensuring quality is <strong>everyone's</strong> responsibility.",
+      preReading: [
+        {
+          link: "http://www.youtube.com/watch?v=T6DJcgm3wNY",
+          title: "Man of Steel trailer"
+        },
+        {
+          link: "http://www.rottentomatoes.com/m/superman_man_of_steel/",
+          title: "Rotten tomatoes review"
+        }
+      ]
+    },
+    {
+      week: "Week 4",
+      summary: "Continuous Integration / Continuous Design",
+      description: "Fail fast! This week is all about integration and deployment, and why both should happen as often as possible. ",
+      preReading: [
+        {
+          link: "http://www.youtube.com/watch?v=T6DJcgm3wNY",
+          title: "Man of Steel trailer"
+        },
+        {
+          link: "http://www.rottentomatoes.com/m/superman_man_of_steel/",
+          title: "Rotten tomatoes review"
+        }
+      ]
+    },
+    {
+      week: "Week 5",
+      summary: "Project work / Guest lectures",
+      description: "Halfway! Now its time for the serious heavy lifting on your project. As project teams, you'll decide what help you need and what (if any) theory you want us to teach to help you get your software over the line.",
+      preReading: [
+        {
+          link: "http://www.youtube.com/watch?v=T6DJcgm3wNY",
+          title: "Man of Steel trailer"
+        },
+        {
+          link: "http://www.rottentomatoes.com/m/superman_man_of_steel/",
+          title: "Rotten tomatoes review"
+        }
+      ]
+    },
+    {
+      week: "Week 6",
+      summary: "Project work / Guest lectures",
+      description: "Halfway! Now its time for the serious heavy lifting on your project. As project teams, you'll decide what help you need and what (if any) theory you want us to teach to help you get your software over the line.",
+      preReading: [
+        {
+          link: "http://www.youtube.com/watch?v=T6DJcgm3wNY",
+          title: "Man of Steel trailer"
+        },
+        {
+          link: "http://www.rottentomatoes.com/m/superman_man_of_steel/",
+          title: "Rotten tomatoes review"
+        }
+      ]
+    },
+    {
+      week: "Week 7",
+      summary: "Project work / Guest lectures",
+      description: "Halfway! Now its time for the serious heavy lifting on your project. As project teams, you'll decide what help you need and what (if any) theory you want us to teach to help you get your software over the line.",
+      preReading: [
+        {
+          link: "http://www.youtube.com/watch?v=T6DJcgm3wNY",
+          title: "Man of Steel trailer"
+        },
+        {
+          link: "http://www.rottentomatoes.com/m/superman_man_of_steel/",
+          title: "Rotten tomatoes review"
+        }
+      ]
+    },
+    {
+      week: "Week 8",
+      summary: "Project work / Guest lectures",
+      description: "Halfway! Now its time for the serious heavy lifting on your project. As project teams, you'll decide what help you need and what (if any) theory you want us to teach to help you get your software over the line.",
+      preReading: [
+        {
+          link: "http://www.youtube.com/watch?v=T6DJcgm3wNY",
+          title: "Man of Steel trailer"
+        },
+        {
+          link: "http://www.rottentomatoes.com/m/superman_man_of_steel/",
+          title: "Rotten tomatoes review"
+        }
+      ]
+    },
+    {
+      week: "Week 9",
+      summary: "Showcase / Retro",
+      description: "Done? Lets see some working software! Time to show off what we've delivered, and then look back to learn from the journey. What have we learned on this project that we could use to do better next time? After that, we'll cap off the semester in style with some social drinks, and celebrate your first software project!",
       preReading: [
         {
           link: "http://www.youtube.com/watch?v=T6DJcgm3wNY",
